379d7f150acaa96d0bb00ab58f68e5751afe847a
Helena Ruiz <hruizrmz@gmail.com>
2020-09-19 18:01:02 -0500

    Organized assets, replaced sprites, and fixed particles

---
+++ hackmit2020/Assets/Scripts/shootingStar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootingStar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 position;
+    float initialX, initialY;
+    void Start()
+    {
+        position = transform.position;
+        initialX = transform.position.x;
+        initialY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position.x += 1f;
+        position.y -= 1f;
+
+        transform.position = position;
+
+        if(position.x >= 855f||position.y <= -533)
+        {
+            position.x = initialX;
+            position.y = initialY;
+            transform.position = position;
+            //System.Threading.Thread.Sleep(1000);
+
+        }
+    }
+}
