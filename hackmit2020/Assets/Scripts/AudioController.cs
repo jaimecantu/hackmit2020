e2ae3984119f15495d3ccbdfde6c3185e537b39b
Helena Ruiz <hruizrmz@gmail.com>
2020-09-19 21:06:21 -0500

    Branch creation
    Organized assets and layers, fixed particle system,  created AudioController

---
+++ hackmit2020/Assets/Scripts/AudioController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioController : MonoBehaviour
+{
+    private static AudioController instance;
+    public AudioSource BGM;
+
+    public AudioClip menuBGM;
+    public AudioClip levelBGM;
+
+    private bool menuLoaded;
+    private bool levelLoaded;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+            menuLoaded = true;
+            levelLoaded = false;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().buildIndex > 1 && !levelLoaded)
+        {
+            BGM.Stop();
+            BGM.clip = levelBGM;
+            BGM.Play();
+            levelLoaded = true;
+            menuLoaded = false;
+        }
+        else if (SceneManager.GetActiveScene().buildIndex <= 1 && !menuLoaded)
+        {
+            BGM.Stop();
+            BGM.clip = menuBGM;
+            BGM.Play();
+            menuLoaded = true;
+            levelLoaded = false;
+        }
+    }
+}
